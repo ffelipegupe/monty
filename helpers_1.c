ee836209004af8b603dac8235a4812a2a78fca92
Felipe Gutierrez <1560@holbertonschool.com>
2020-05-14 18:33:01 -0500

    Changing: helpers_1.c

---
+++ helpers_1.c
@@ -0,0 +1,31 @@
+#include "monty.h"
+/**
+ * parse - function that parses strings into tokens
+ * @buff: buffer
+ * @stack: stack
+ * @line_number: number of lines
+ *
+ * Return: nothing
+ */
+
+void parse(char *buff, stack_t **stack, unsigned int line_number)
+{
+	char *chn, *tmp;
+
+	chn = strtok(buff, " \n\t");
+	tmp = strtok(NULL, " \n\t");
+	glob.data = tmp;
+	if (chn && chn[0] != '#')
+		get_opcode(stack, line_number, chn);
+}
+
+/**
+ * check_digit - function that checks a digit existance
+ * @chr: character to be checked
+ *
+ * Return: 0, on success; 1, otherwise
+ */
+int check_digit(int chr)
+{
+	return ((c >= '0' && c <= '9') ? 1 : 0);
+}
